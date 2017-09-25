3c737a446d5f7830e2a76cf4287d21a5cfc2cdb7
Simon Torres <storres@ctio.noao.edu>
2017-09-25 10:01:16 -0300

    updated plotting tool

--- pipeline_to_IRAF_comparison/plot_iraf_pipeline.py
+++ pipeline_to_IRAF_comparison/plot_iraf_pipeline.py
@@ -3,32 +3,43 @@ import astropy.units as u
 from ccdproc import CCDData
 from goodman_spec.wsbuilder import ReadWavelengthSolution
 
+offset = 6000.
 
-iraf_file = './files/CVSO-114SW_400M2_comb_1d_wl.fits'
-pipe_file = './files/auto_gcfzsto_0074.cvso114_400M2_GG455_1.fits'
+iraf_file = '/data/simon/development/soar/goodman-hts-general-tools/pipeline_to_IRAF_comparison/files/CVSO-114SW_400M2_comb_1d_wl.fits'
+pipe_file = '/data/simon/development/soar/goodman-hts-general-tools/pipeline_to_IRAF_comparison/files/auto_gcfzsto_0074.cvso114_400M2_GG455_1.fits'
 
 iraf_data = CCDData.read(iraf_file, unit=u.adu)
 
-iraf_ws = ReadWavelengthSolution(iraf_data.header, iraf_data.data)
+iraf_ws = ReadWavelengthSolution(header=iraf_data.header,
+                                 data=iraf_data.data)
 
 pipe_data = CCDData.read(pipe_file, unit=u.adu)
 
-pipe_ws = ReadWavelengthSolution(pipe_data.header, pipe_data.data)
+pipe_ws = ReadWavelengthSolution(header=pipe_data.header,
+                                 data=pipe_data.data)
 
 
-iraf = iraf_ws()
+iraf_w, iraf_i = iraf_ws()
 
-pipe = pipe_ws()
+iraf_oi = iraf_i + offset
 
-wav_min = min([iraf[0][0], pipe[0][0]])
+pipe_w, pipe_i = pipe_ws()
 
-wav_max = max([iraf[0][-1], pipe[0][-1]])
+wav_min = min([iraf_w[0], pipe_w[0]])
+
+wav_max = max([iraf_w[-1], pipe_w[-1]])
 print(wav_min, wav_max)
 
 plt.xlim((wav_min, wav_max))
 
-plt.plot(iraf[0], iraf[1], label='IRAF Solved', color='k', alpha=.7)
-plt.plot(pipe[0], pipe[1], label='Goodman Pipeline', color='k')
 plt.axvline(6562.8, color='r', label='H-alpha 6562.8A')
+plt.plot(iraf_w, iraf_oi, label='IRAF Solved', color='k', alpha=.5)
+plt.plot(pipe_w, pipe_i, label='Goodman Pipeline', color='k')
+plt.title('Pipeline V/S IRAF comparison\nCVSO-114\n400M2 + GG455')
+plt.xlabel('Wavelength (Angstrom)')
+plt.ylabel('Intensity (ADU)')
 plt.legend(loc='best')
+plt.tight_layout()
+
+plt.savefig('/data/simon/development/soar/goodman-hts-general-tools/pipeline_to_IRAF_comparison/files/pipe_iraf.png', dpi=300)
 plt.show()
